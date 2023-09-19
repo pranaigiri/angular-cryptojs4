173fe2564576dad6157aa73d6ea402a6ae55bcb7
Pranai Giri <pranaigirisikkim@gmail.com>
2023-09-19 17:32:28 +0530

    Added endpoint compatible for ASP.Net Web Api

--- src/services/encryption-decryption.service.ts
+++ src/services/encryption-decryption.service.ts
@@ -20,4 +20,38 @@ decrypt(encryptedText:string){
   return CryptoJS.AES.decrypt(encryptedText, environment.aesKey).toString(CryptoJS.enc.Utf8);
 }
 
+
+
+// -------------------------------------------------------------- COMPATIBLE WITH ASP.NET WEB API
+
+key = CryptoJS.enc.Utf8.parse(environment.aesKey);
+iv  = CryptoJS.enc.Utf8.parse(environment.iv);
+
+// Methods for the encrypt and decrypt Using AES
+encryptForAsp(data:any) {
+  let encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(JSON.stringify(data)), this.key, {
+      keySize: 128 / 8,
+      iv: this.iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7
+  });
+  console.log('Encrypted :' + encrypted);
+  this.decryptForAsp(encrypted);
+  return encrypted;
+}
+
+decryptForAsp(decString:any) {
+  var decrypted = CryptoJS.AES.decrypt(decString, this.key, {
+      keySize: 128 / 8,
+      iv: this.iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7
+  });
+  console.log('Decrypted : ' + decrypted);
+  console.log('utf8 = ' + decrypted.toString(CryptoJS.enc.Utf8));
+
+  return decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+
 }
